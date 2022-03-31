80ca3733b5803aecf52f69735f31bdf772c4287d
Ravaux Nicolas <la208319@student.helha.be>
2022-03-31 11:26:02 +0200

    ADD menuDropdown

---
+++ app/views/include/menuDropdown.php
@@ -0,0 +1,38 @@
+<nav class="navigation menuDropdown">
+    <div class="nav-brand">
+        <img src="../../assets/img/home.png">
+    </div>
+    <ul>
+        <li class="list active">
+            <a href="#">
+                <span class="text">
+                    menu 1
+                </span>
+            </a>
+        </li>
+        <li class="list">
+            <a href="#">
+                <span class="text">
+                    menu 2
+                </span>
+            </a>
+        </li>
+        <li class="list">
+            <a href="#">
+                <span class="text">
+                    menu 3
+                </span>
+            </a>
+        </li>
+        <li class="list">
+            <a href="#">
+                <span class="text">
+                    menu 4
+                </span>
+            </a>
+        </li>
+    </ul>
+    <div class="nav-profile">
+        <img src="../../assets/img/home.png">
+    </div>
+</nav>
---
+++ app/views/include/tabbar.php
@@ -0,0 +1,39 @@
+<nav class="fixed-bottom">
+    <div class="navigation tabbar">
+        <ul>
+            <li class="list active">
+                <a href="#">
+                    <span class="icon">
+                        <img src="../../assets/img/home.png">
+                    </span>
+                    <span class="text">Home</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon">
+                        <img src="../../assets/img/home.png">
+                    </span>
+                    <span class="text">CV</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon">
+                        <img src="../../assets/img/home.png">
+                    </span>
+                    <span class="text">Projets</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon">
+                        <img src="../../assets/img/home.png">
+                    </span>
+                    <span class="text">Profil</span>
+                </a>
+            </li>
+            <div class="indicator"></div>
+        </ul>
+    </div>
+</nav>
