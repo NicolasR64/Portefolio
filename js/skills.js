fc7d4cf66e6c466b779f8e281ec6614aeeae4eb0
NicolasR64 <nicolasravaux64@gmail.com>
2026-02-01 14:12:24 +0100

    Add logo

--- js/skills.js
+++ js/skills.js
@@ -30,7 +30,9 @@ function onInit_skills(){
 					skills += `
 					<div class="skill">
 						<a href="${skill.link}" target="_blank">
-							<img src="${skill.logo}">
+							<div class="skill-logo">
+								<img src="${skill.logo}">
+							</div>
 							<span>${skill.name}</span>
 						</a>
 					</div>`;
@@ -75,6 +77,8 @@ function onInit_skills(){
 		dynamicHeightSection();
 	}
 
+		
+	// 3. Generate skills from JSON
 	generate_skills(JSON_SKILLS)
 }
 
@@ -87,54 +91,59 @@ JSON_SKILLS = {
 			[
 				{
 					"name": "HTML",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+					"logo": "assets/img/logo/html.svg",
 					"link": "https://en.wikipedia.org/wiki/HTML"
 				},
 				{
 					"name": "CSS",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+					"logo": "assets/img/logo/css.svg",
 					"link": "https://en.wikipedia.org/wiki/CSS"
 				},
 				{
 					"name": "JavaScript",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+					"logo": "assets/img/logo/javascript.png",
 					"link": "https://en.wikipedia.org/wiki/JavaScript"
 				},
 				{
+					"name": "wLangage",
+					"logo": "assets/img/logo/windev.png",
+					"link": "https://doc.pcsoft.fr/fr-FR/?9000196&name=wlangage"
+				},
+				{
 					"name": "TypeScript",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+					"logo": "assets/img/logo/typescript.svg",
 					"link": "https://www.typescriptlang.org"
 				},
 				{
-					"name": "jQuery",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+					"name": "JQuery",
+					"logo": "assets/img/logo/jquery.png",
 					"link": "https://jquery.com/"
 				},
 				{
 					"name": "Bootstrap",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+					"logo": "assets/img/logo/bootstrap.svg",
 					"link": "https://getbootstrap.com/"
 				},
 				{
 					"name": "Angular",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+					"logo": "assets/img/logo/angular.png",
 					"link": "https://angular.dev"
 				},
 				{
 					"name": "Ang. Material",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+					"logo": "assets/img/logo/angular.png",
 					"link": "https://material.angular.io"
 				},
-				{
-					"name": "Git",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
-					"link": "https://git-scm.com/"
-				},
-				{
-					"name": "PHP",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
-					"link": "https://www.php.net"
-				}
+				// {
+				// 	"name": "Git",
+				// 	"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+				// 	"link": "https://git-scm.com/"
+				// },
+				// {
+				// 	"name": "PHP",
+				// 	"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+				// 	"link": "https://www.php.net"
+				// }
 			]
 		},
 		{
@@ -142,86 +151,82 @@ JSON_SKILLS = {
 			"skills" :
 			[
 				{
-					"name": "JavaScript",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
-					"link": "https://en.wikipedia.org/wiki/JavaScript"
-				},
-				{
-					"name": "TypeScript",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
-					"link": "https://www.typescriptlang.org"
-				},
-				{
 					"name": "DiscordJS",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+					"logo": "assets/img/logo/discord.svg",
 					"link": "https://discord.js.org"
 				},
-				{
-					"name": "C#",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
-					"link": "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)"
-				},
+				// {
+				// 	"name": "C#",
+				// 	"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+				// 	"link": "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)"
+				// },
 				{
 					"name": "SQL",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
-					"link": "https://sql.sh"
+					"logo": "assets/img/logo/sql.png",
+					"link": "https://en.wikipedia.org/wiki/SQL"
 				},
-				{
-					"name": "PostGresSQL",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
-					"link": "https://www.postgresql.org"
-				},
-				{
-					"name": "Stripe",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
-					"link": "https://stripe.com/"
-				},
+				// {
+				// 	"name": "PostGresSQL",
+				// 	"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+				// 	"link": "https://www.postgresql.org"
+				// },
+				// {
+				// 	"name": "Stripe",
+				// 	"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+				// 	"link": "https://stripe.com/"
+				// },
 				{
 					"name": "Git",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+					"logo": "assets/img/logo/git.svg",
 					"link": "https://git-scm.com/"
 				},
+				// {
+				// 	"name": "Spring boot",
+				// 	"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+				// 	"link": "https://spring.io/projects/spring-boot"
+				// },
+				// {
+				// 	"name": "PHP",
+				// 	"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+				// 	"link": "https://www.php.net"
+				// }
 				{
-					"name": "Spring boot",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
-					"link": "https://spring.io/projects/spring-boot"
+					"name": "SSMS",
+					"logo": "assets/img/logo/ssms.png",
+					"link": "https://learn.microsoft.com/en-us/ssms/"
 				},
-				{
-					"name": "PHP",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
-					"link": "https://www.php.net"
-				}
 			]
 		},
 		{
 			"name" : "Software Development",
 			"skills" :
 			[
+				// {
+				// 	"name": "C",
+				// 	"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+				// 	"link": "https://en.wikipedia.org/wiki/C_(programming_language)"
+				// },
+				// {
 				{
-					"name": "C",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
-					"link": "https://en.wikipedia.org/wiki/C_(programming_language)"
-				},
-				{
-					"name": "Windev",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
-					"link": "https://pcsoft.fr"
+					"name": "windev",
+					"logo": "assets/img/logo/windev.png",
+					"link": "https://doc.pcsoft.fr/fr-FR/?9000196&name=wlangage"
 				},
 				{
 					"name": "Java",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+					"logo": "assets/img/logo/java.svg",
 					"link": "https://en.wikipedia.org/wiki/Java_(programming_language)"
 				},
-				{
-					"name": "C#",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
-					"link": "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)"
-				},
-				{
-					"name": "Git",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
-					"link": "https://git-scm.com/"
-				}
+				// {
+				// 	"name": "C#",
+				// 	"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+				// 	"link": "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)"
+				// },
+				// {
+				// 	"name": "Git",
+				// 	"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+				// 	"link": "https://git-scm.com/"
+				// }
 			]
 		},
 		{
@@ -230,12 +235,12 @@ JSON_SKILLS = {
 			[
 				{
 					"name": "Figma",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+					"logo": "assets/img/logo/figma.svg",
 					"link": "https://www.figma.com/"
 				},
 				{
 					"name": "Canva",
-					"logo": "https://upload.wikimedia.org/wikipedia/commons/archive/3/35/20220802133510%21The_C_Programming_Language_logo.svg",
+					"logo": "assets/img/logo/canva.svg",
 					"link": "https://www.canva.com/"
 				}
 			]
